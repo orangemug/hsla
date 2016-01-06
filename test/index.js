6f2b55eef7a5fb4d3fbe5f077c682535b79f5bd0
orangemug <info@orangemug.co.uk>
2016-01-06 11:33:33 +0000

    Added full set of tests.

--- test/index.js
+++ test/index.js
@@ -29,7 +29,7 @@ describe("hsla", function() {
   describe("manipulation", function() {
     var c, cv;
     before(function() {
-      c = hsla();
+      c = hsla(60, 80, 50, 1);
       cv = c.toString();
     })
 
@@ -38,85 +38,135 @@ describe("hsla", function() {
       it("should return new hsla with alpha set", function() {
         var nc = c.alpha(0.5)
         assert.notEqual(nc.toString(), c.toString());
-        assert.equal(nc, "hsla(0, 100%, 50%, 0.5)");
+        assert.equal(nc, "hsla(60, 80%, 50%, 0.5)");
       })
       it("should not allow <0", function() {
         var nc = c.alpha(-1.1);
-        assert.equal(nc, "hsla(0, 100%, 50%, 0)");
+        assert.equal(nc, "hsla(60, 80%, 50%, 0)");
       });
       it("should not allow >1", function() {
         var nc = c.alpha(1.1);
-        assert.equal(nc, "hsla(0, 100%, 50%, 1)");
-      });
-    });
-
-    describe("#lighten", function() {
-      it("should return new hsla lightened by percent", function() {
-      });
-      it("should not allow >100", function() {
-      });
-    });
-
-    describe("#darken", function() {
-      it("should return new hsla darkened by percent", function() {
-      });
-      it("should not allow <0", function() {
+        assert.equal(nc, "hsla(60, 80%, 50%, 1)");
       });
     });
 
     describe("#saturate", function() {
       it("should return new hsla saturated by percent", function() {
+        var nc = c.saturate(20);
+        assert.equal(nc.toString(), "hsla(60, 100%, 50%, 1)");
       });
       it("should not allow >100", function() {
+        var nc = c.saturate(20);
+        assert.equal(nc, "hsla(60, 100%, 50%, 1)");
       });
     });
 
     describe("#desaturate", function() {
       it("should return new hsla desaturated by percent", function() {
+        var nc = c.desaturate(20);
+        assert.equal(nc, "hsla(60, 60%, 50%, 1)");
       });
       it("should not allow <0", function() {
+        var nc = c.desaturate(110);
+        assert.equal(nc, "hsla(60, 0%, 50%, 1)");
+      });
+    });
+
+    describe("#lighten", function() {
+      it("should return new hsla lightened by percent", function() {
+        var nc = c.lighten(20);
+        assert.equal(nc, "hsla(60, 80%, 70%, 1)");
+      });
+      it("should not allow >100", function() {
+        var nc = c.lighten(90);
+        assert.equal(nc, "hsla(60, 80%, 100%, 1)");
+      });
+    });
+
+    describe("#darken", function() {
+      it("should return new hsla darkened by percent", function() {
+        var nc = c.darken(20);
+        assert.equal(nc, "hsla(60, 80%, 30%, 1)");
+      });
+      it("should not allow <0", function() {
+        var nc = c.darken(90);
+        assert.equal(nc, "hsla(60, 80%, 0%, 1)");
       });
     });
 
     describe("#rotate", function() {
       it("should return new hsla rotated by degrees", function() {
+        var nc1 = c.rotate(-30);
+        var nc2 = c.rotate(+30);
+
+        assert.equal(nc1.toString(), "hsla(30, 80%, 50%, 1)");
+        assert.equal(nc2.toString(), "hsla(90, 80%, 50%, 1)");
       });
       it("should wrap if <0", function() {
+        var nc = c.rotate(-90);
+        assert.equal(nc.toString(), "hsla(330, 80%, 50%, 1)");
       });
       it("should wrap if >359", function() {
+        var nc = c.rotate(300);
+        assert.equal(nc.toString(), "hsla(0, 80%, 50%, 1)");
       });
     });
 
     describe("#triadic", function() {
-      it("should...", function() {
+      it("should work", function() {
+        var colors = c.triadic();
+        assert.equal(colors[0], "hsla(180, 80%, 50%, 1)");
+        assert.equal(colors[1], "hsla(60, 80%, 50%, 1)");
+        assert.equal(colors[2], "hsla(300, 80%, 50%, 1)");
       });
     });
 
     describe("#analogous", function() {
-      it("should...", function() {
+      it("should work", function() {
+        var colors = c.analogous();
+        assert.equal(colors[0], "hsla(45, 80%, 50%, 1)");
+        assert.equal(colors[1], "hsla(60, 80%, 50%, 1)");
+        assert.equal(colors[2], "hsla(75, 80%, 50%, 1)");
       });
     });
 
     describe("#opposite", function() {
-      it("should...", function() {
+      it("should work", function() {
+        var nc = c.opposite();
+        assert.equal(nc, "hsla(240, 80%, 50%, 1)");
       });
     });
   });
 
 
   describe("util", function() {
+    var c, cv;
+    before(function() {
+      c = hsla(60, 80, 50, 1);
+      cv = c.toString();
+    })
+
     describe("#clone", function() {
       it("should make new object", function() {
+        var nc = c.clone()
+        assert.notEqual(nc, c);
       });
     });
 
     describe("#toString", function() {
       it("should return hsla string", function() {
+        assert.equal(c.toString(), "hsla(60, 80%, 50%, 1)");
       });
     });
 
     describe("#toJSON", function() {
       it("should return javascript object", function() {
+        assert.deepEqual(c.toJSON(), {
+          hue: 60,
+          saturation: 80,
+          lightness: 50,
+          alpha: 1
+        });
       });
     });
   });
